2be2322d5d360ba2d6e35cb16e30e7399985adf7
Arthur Nakayama <arthurnakayama@gmail.com>
2025-11-22 20:00:37 -0300

    Feat: Reformulação da Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def show
+    @questionnaires = Questionnaire
+                          .where(deleted_at: nil)
+                          .order("RANDOM()")
+                          .limit(6)
   end
 end
